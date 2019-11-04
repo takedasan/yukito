b7306f314aa408f07aeb144f6e28905b1fafe3a9
takedasan <takedasan593@gmail.com>
2019-11-04 12:43:16 +0900

    firebase設定の追加

---
+++ functions/src/index.ts
@@ -0,0 +1,5 @@
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
+
+const nuxt = new Nuxt({ buildDir: 'ssr', dev: false })
+exports.ssr = functions.https.onRequest(nuxt.render)
